a081b0dba0b0b86de43f0de0a7bec063c71189ae
Lucas Wang <lucas.l.wang@gmail.com>
2023-12-04 16:18:16 -0800

    2 function

---
+++ cmake-build-debug/CMakeFiles/Week10DI.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Week10DI.dir/main.cpp.obj"
+  "CMakeFiles/Week10DI.dir/main.cpp.obj.d"
+  "Week10DI.exe"
+  "Week10DI.exe.manifest"
+  "Week10DI.pdb"
+  "libWeek10DI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Week10DI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
